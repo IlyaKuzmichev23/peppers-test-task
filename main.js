fa69eb6903caa6d60fbb09991587a12e59caf372
IlyaKuzmichev23 <108921415+IlyaKuzmichev23@users.noreply.github.com>
2026-05-23 01:54:38 +0300

    добавлены платформа и шар. выполнена обработка столкновения шара со стенами и платформой

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { Application} from "pixi.js";
+import { Application, Graphics} from "pixi.js";
 
 export class Game {
     constructor(){
@@ -10,14 +10,46 @@ export class Game {
 
         await this.app.init({
             width: 800,
-            height: 600,
-            background: 0x000000,
+            height: 900,
+            background: 0x808080,
             antialias:  true,
             resolution: window.devicePixelRatio || 1,
-            autoDensity: true,       // масштабирование под Retina
+            autoDensity: true,
         });
 
         document.body.appendChild(this.app.canvas);
+
+        //платформа
+        const paddle = new Graphics();
+        paddle.rect(0,0,130,20);
+        paddle.x = 335;
+        paddle.y = 800;
+        paddle.fill(0x273591);
+        this.app.stage.addChild(paddle);
+
+        //шар
+        const ball = new Graphics();
+        ball.circle(0,0,10);
+        ball.x = 400;
+        ball.y = 790;
+        ball.fill(0xd4d00f);
+        this.app.stage.addChild(ball);
+
+        //игровой цикл
+        let vx = 5;
+        let vy = -5;
+        this.app.ticker.add((time) => {
+            ball.x+=vx*time.deltaTime;
+            ball.y+=vy*time.deltaTime;
+            console.log(ball.y);
+            if(ball.x-10<=0 || ball.x+10>=800)
+                vx*=-1;
+            if(ball.y-10<=0)
+                vy*=-1;
+            if(ball.y+10>=paddle.y)
+                vy*=-1;
+        });
+
     };
 
 }
