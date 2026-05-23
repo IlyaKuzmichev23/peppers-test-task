1868bfc854d679528415c41c83863de90e263d53
IlyaKuzmichev23 <108921415+IlyaKuzmichev23@users.noreply.github.com>
2026-05-23 23:55:58 +0300

    Реализован функциональный полдход.Добавлено движение платформы и отрисовка блоков

--- main.js
+++ main.js
@@ -1,10 +1,81 @@
-import { Application, Graphics} from "pixi.js";
+import { Application, effectsMixin, Graphics} from "pixi.js";
 
 export class Game {
     constructor(){
         this.app = null;
     }
 
+    create_paddle() {
+        this.paddle = new Graphics();
+        this.paddle.rect(0,0,130,20);
+        this.paddle.x = 335;
+        this.paddle.y = 800;
+        this.paddle.fill(0x273591);
+        this.app.stage.addChild(this.paddle);
+    }
+
+    create_ball(){
+        this.ball = new Graphics();
+        this.ball.circle(0,0,10);
+        this.ball.x = 400;
+        this.ball.y = 790;
+        this.ball.fill(0xd4d00f);
+        this.app.stage.addChild(this.ball);
+    }
+
+    game_cycle(){
+        this.vx = 5;
+        this.vy = -5;
+        this.app.ticker.add((time) => {
+            this.ball.x+=this.vx*time.deltaTime;
+            this.ball.y+=this.vy*time.deltaTime;
+            if(this.ball.x-10<=0 || this.ball.x+10>=800)
+                this.vx*=-1;
+            if(this.ball.y-10<=0)
+                this.vy*=-1;
+            if((this.ball.y+10>=this.paddle.y && this.ball.y<this.paddle.y) && (this.ball.x>=this.paddle.x && this.ball.x<=this.paddle.x+130))
+                this.vy*=-1;
+            if(this.move_left && this.paddle.x>0)
+                this.paddle.x -= 5*time.deltaTime;
+            if(this.move_right && this.paddle.x+130<800)
+                this.paddle.x += 5*time.deltaTime;
+        });
+    };
+
+    move_paddle(){
+        this.move_left = false;
+        this.move_right = false;
+        window.addEventListener("keydown", (e)=>{
+            if(e.code=="ArrowLeft")
+                this.move_left = true;
+            if(e.code=="ArrowRight")
+                this.move_right = true;   
+        });
+        window.addEventListener("keyup", (e)=>{
+            if(e.code=="ArrowLeft")
+                this.move_left = false;
+            if(e.code=="ArrowRight")
+                this.move_right = false;   
+        });
+    };
+
+    create_blocks(){
+        this.blocks = [];
+        let start_x = 12;
+        let start_y = 100;
+        for(let i = 0; i<5; i++){
+            for(let j = 0; j<11; j++){
+                let block = new Graphics();
+                block.rect(start_x, start_y, 66, 30);
+                block.fill(Math.random() * 0xFFFFFF);
+                this.app.stage.addChild(block);
+                this.blocks.push(block);
+                start_x+=71;
+            };
+            start_x = 12;
+            start_y+=35;
+        };
+    }
     async init(){
         this.app = new Application();
 
@@ -20,35 +91,19 @@ export class Game {
         document.body.appendChild(this.app.canvas);
 
         //платформа
-        const paddle = new Graphics();
-        paddle.rect(0,0,130,20);
-        paddle.x = 335;
-        paddle.y = 800;
-        paddle.fill(0x273591);
-        this.app.stage.addChild(paddle);
+        this.create_paddle();
 
         //шар
-        const ball = new Graphics();
-        ball.circle(0,0,10);
-        ball.x = 400;
-        ball.y = 790;
-        ball.fill(0xd4d00f);
-        this.app.stage.addChild(ball);
+        this.create_ball();
+
+        //движение платформы
+        this.move_paddle();
+
+        //создание блоков
+        this.create_blocks();
 
         //игровой цикл
-        let vx = 5;
-        let vy = -5;
-        this.app.ticker.add((time) => {
-            ball.x+=vx*time.deltaTime;
-            ball.y+=vy*time.deltaTime;
-            console.log(ball.y);
-            if(ball.x-10<=0 || ball.x+10>=800)
-                vx*=-1;
-            if(ball.y-10<=0)
-                vy*=-1;
-            if(ball.y+10>=paddle.y)
-                vy*=-1;
-        });
+        this.game_cycle();
 
     };
 
